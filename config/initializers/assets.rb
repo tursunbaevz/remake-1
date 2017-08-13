f35a27ded7679f6dae6d36e4742adf98ab1c4331
tursunbaevz <zuma9405@gmail.com>
2017-08-13 18:32:29 +0600

    precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( admin/admin.css )
 Rails.application.config.assets.precompile += %w( admin/admin.js )
+
+Rails.application.config.assets.precompile += [/^[-_a-zA-Z0-9]*\..*/]
